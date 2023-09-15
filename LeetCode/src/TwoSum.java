9fd5c4d653590b2c82dbbba88cd7d0a7e7db6208
Loshan Sundaramoorthy <losh0001@gmail.com>
2023-09-15 22:16:00 +0100

    Programming Exercises

---
+++ LeetCode/src/TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[]{i, j};
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solutions");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+
+    }
+}
